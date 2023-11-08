54ac41a9ed33deabc12695d994cb9535b780a133
NatashaBertaina <n.bertaina@alumno.um.edu.ar>
2023-11-08 09:38:27 -0300

    session

--- trainingApp/views.py
+++ trainingApp/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic, View
 from django.views.generic.edit import FormView
 from random import shuffle, choice
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import QuestionForm
 from .models import Training, Deploy
@@ -27,10 +28,9 @@ class DeployDetailView(View):
         form = QuestionForm(instance=deploy)
 
         #crear una lista random con los despliegues
-        rnd_deploy = list(Deploy.objects.values_list('pk', flat=True))
-        request.session['rnd_deploy'] = random.shuffle(rnd_deploy)
+        request.session['rnd_deploy'] = list(Deploy.objects.values_list('pk', flat=True))
         #Guardo el índice de la lista
-        request.session['index'] = 0
+        request.session['index'] = request.session.get('index', 0)
 
 
         return render(request, self.template_name, {'deploy': deploy, 'form': form})
@@ -40,7 +40,7 @@ class DeployDetailView(View):
         form = QuestionForm(request.POST, instance=deploy)
         if form.is_valid():
             form.save()
-
+            return redirect('next_deploy_view')
             #Redirijimos al usuario al siguiente depliegue
             #next_deploy = self.get_next_deploy(deploy)
             #if next_deploy:
@@ -52,28 +52,49 @@ class DeployDetailView(View):
             #Con lo siguiente, si el formulario no es válido, renderizaré la plantilla con el formulario una vez más, resaltando lo que falta para poder enviarlo.
             return render(request, self.template_name, {'deploy':deploy, 'form':form})
 
-    def next_deploy(request):
-        if 'rnd_deploy' in request.session and 'index' in request.session:
-            deploy_new = request.session['rnd_deploy']
-            index = request.session['index']
+def next_deploy(request):
+    rnd_deploy = request.session.get('rnd_deploy')
+    index = request.session.get('index', 0)
 
-            if index < len(deploy_new) - 1:
-                request.session['index'] += 1
-            
-        return render(request, 'forms.html')
+    if rnd_deploy and index < len(rnd_deploy) - 1:
+        request.session['index'] += 1
+        next_deploy_id = rnd_deploy[request.session['index']]
+        return redirect('forms', deploy_id=next_deploy_id)
+    else: 
+        return redirect('results')
+
+def prev_deploy(request):
+    index = request.session.get('index', 0)
+    rnd_deploy = request.session.get('rnd_deploy')
     
-    def prev_deploy(request):
-        if 'rnd_deploy' in request.session and 'index' in request.session:
-            index = request.session['index']
+    if rnd_deploy and index > 0:
+        request.session['index'] -= 1
+        prev_deploy_id = rnd_deploy[request.session['index']]
+        return redirect('forms', deploy_id=prev_deploy_id)
+    else:
+        return redirect('forms', deploy_id=request.session['rnd_deploy'][index])
 
-            if index > 0:
-                request.session['index'] -= 1
-        
-        return render(request, 'forms.html')
+
+
+def results(request):
+    rnd_deploy = request.session.get('rnd_deploy')
+    if rnd_deploy:
+        paginator = Paginator(rnd_deploy, 1)
+        page = request.GET.get('page')
+        try:
+            deploy_list = paginator.page(page)
+        except PageNotAnInteger:
+            deploy_list = paginator.page(1)
+        except EmptyPage:
+            deploy_list = paginator.page(paginator.num_pages)
+
+        return render(request, 'result.html', {'deploy_list':deploy_list})
+    
+    else:
+        return render
 
 
 class ResultsView(generic.DetailView):
     model = Training
     template_name = "trainingApp/results.html"
-
 
