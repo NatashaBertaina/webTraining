40ed46d08399a309fe4e498157de6d3d2289dced
NatashaBertaina <n.bertaina@alumno.um.edu.ar>
2023-12-24 09:46:25 -0300

    modificacion de modelos

--- trainingApp/models.py
+++ trainingApp/models.py
@@ -5,26 +5,64 @@ from django.utils import timezone
 from django.contrib import admin
 
 class Training(models.Model):
+    difficulty = {
+        'B' : 'Beginner',
+        'M' : 'Medium',
+        'A' : 'Advanced',
+    }
+
     name_training = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("upload date")
+    label = models.CharField(max_length=1, choices=difficulty)
 
-
-    def was_published_recently(self):
-        return self.pub_date >=timezone.now() - datetime.timedelta(days=1)
     def __str__(self):
         return self.name_training
 
 
-class Deploy(models.Model):
-    training = models.ForeignKey(Training, on_delete=models.CASCADE)
+class Block(models.Model):
+    Training = models.ForeignKey(Training, on_delete=models.CASCADE)
+    name_block = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+class Deploy_a(models.Model):
+    training = models.ForeignKey(Block, on_delete=models.CASCADE)
     deploy_image = models.ImageField(upload_to="trainingApp/images", blank=True)
     deploy_sound = models.FileField(upload_to="trainingApp/sound", blank=True)
+
     ch_d1 = models.CharField(max_length=50, null=True)
     ch_d2 = models.CharField(max_length=50, null=True)
     ch_d3 = models.CharField(max_length=50, null=True)
     ch_d4 = models.CharField(max_length=50, null=True)
 
-    user_response = models.CharField(max_length=50, null=True)
+    
 
     def __str__(self):
         return f"Deploy: {self.id}"
+
+
+class Deploy_b(models.Model):
+    training = models.ForeignKey(Block, on_delete=models.CASCADE)
+    deploy_image = models.ImageField(upload_to="trainingApp/images", blank=True)
+    deploy_sound = models.FileField(upload_to="trainingApp/sound", blank=True)
+
+    ch_d1 = models.CharField(max_length=50, null=True)
+    ch_d2 = models.CharField(max_length=50, null=True)
+    ch_d3 = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"Deploy: {self.id}"
+    
+
+
+class DeployAnswerA(models.Model):
+    question = models.ForeignKey(Deploy_a, on_delete=models.CASCADE)
+    user_response = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.user_response
+
+class DeployAnswerB(models.Model):
+    question = models.ForeignKey(Deploy_b, on_delete=models.CASCADE)
+    user_response = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.user_response
