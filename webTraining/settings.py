f9e862c95c4f7a4628e09789113813b41f48131b
NatashaBertaina <n.bertaina@alumno.um.edu.ar>
2024-02-22 12:33:52 -0300

    production

--- webTraining/settings.py
+++ webTraining/settings.py
@@ -50,14 +50,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    #Para archivos estáticos en producción
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #Para archivos estáticos en producción
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'webTraining.urls'
@@ -131,7 +131,8 @@ USE_TZ = True
 
 #Configuracion de archivos estáticos
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #Config para archivos de audio e imagenes de la base de datos
