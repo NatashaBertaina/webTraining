3b161fde7f1b4a9d819f69e4a0ea8399626343dc
Gonzalo Cayo <gonzacayo@gmail.com>
2024-09-04 16:13:15 -0300

    Traduccion Estados BlockAnswer

--- trainingApp/templatetags/custom_filters.py
+++ trainingApp/templatetags/custom_filters.py
@@ -1,6 +1,6 @@
 # trainingApp/templatetags/custom_filters.py
 from django import template
-
+from django.utils.translation import gettext_lazy as _
 register = template.Library()
 
 #Metodo para obtener la clave de un diccionario
@@ -12,3 +12,7 @@ def get_value(dictionary, key):
 @register.filter
 def get_state(dictionary, key):
     return dictionary[f'{key}']
+
+@register.filter
+def get_state_trans(dictionary, key):
+    return _(dictionary[f'{key}'])
